b4fe1d5149bc73ed5e770c4fec55b474084084c0
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-09-15 18:54:57 +0530

    Add files

---
+++ InputToSender/SenderTests/ControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using Xunit;
+namespace InputToSender.SenderTests
+{
+    public class ControllerTests
+    {
+        [Fact]
+        public static void TestExpectingAnObjectOfCSVInputTypeToBeAssignedToControllersInputInterface()
+        {
+
+            CSVInput csvInput = new CSVInput("SampleTest.csv");
+            ConsoleOutput output = new ConsoleOutput();
+            Controller controller = new Controller(csvInput, output);
+            var type = controller.inputInterface.GetType();
+            Debug.Assert(type == csvInput.GetType());
+        }
+    }
+}
