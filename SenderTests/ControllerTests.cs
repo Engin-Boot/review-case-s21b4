fa1d5d6cb3e2894f970e78aedcd9a8c627e658f4
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-09-15 19:20:20 +0530

    Add files

---
+++ SenderTests/ControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using Xunit;
+using InputToSender;
+namespace InputToSender.SenderTests
+{
+    public class ControllerTests
+    {
+        [Fact]
+        public static void TestExpectingAnObjectOfCSVInputTypeToBeAssignedToControllersInputInterface()
+        {
+
+            CSVInput csvInput = new CSVInput("TestSample.csv");
+            ConsoleOutput output = new ConsoleOutput();
+            Controller controller = new Controller(csvInput, output);
+            var type = controller.inputInterface.GetType();
+            Debug.Assert(type == csvInput.GetType());
+        }
+    }
+}
---
+++ SenderTests/SenderOutputTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+namespace InputToSender.SenderTests
+{
+    class MockConsoleOuput : ISenderOutput
+    {
+        internal List<List<String>> OutputOnConsole = new List<List<string>>();
+
+        public void WriteOutput(IEnumerable<IEnumerable<String>> data)
+        {
+            List<String> newRow;
+            foreach (IEnumerable<String> row in data)
+            {
+                newRow = new List<string>();
+                foreach (String value in row)
+                {
+                    newRow.Add(value);
+                }
+                OutputOnConsole.Add(newRow);
+            }
+        }
+    }
+
+    public class SenderUnitTest
+    {
+        [Fact]
+        public void WhenCalledWithTwoDimensionalDataThenAccessValuesRowWiseFromData()
+        {
+            List<List<String>> testinput = new List<List<string>>
+            {
+                new List<string> { "Date", "Comment" },
+                new List<string> { "12/12/2012", "All good" },
+                new List<string> { "11/11/2011", "Remove duplication" },
+                new List<string> { "30/11/2015", "Edge Cases not handled" }
+            };
+
+            MockConsoleOuput mockConsoleOuput = new MockConsoleOuput();
+            mockConsoleOuput.WriteOutput(testinput);
+
+            List<List<String>> testoutput = mockConsoleOuput.OutputOnConsole;
+            Assert.Equal(testinput, testoutput);
+        }
+
+        [Fact]
+        public void WhenCalledWithTwoDimensionalDataThenReturnNumberofColumnsinData()
+        {
+            List<List<String>> testinput = new List<List<string>>
+            {
+                new List<string> { "Date", "Comment" },
+                new List<string> { "12/12/2012", "All good" },
+                new List<string> { "11/11/2011", "Remove duplication" },
+                new List<string> { "30/11/2015", "Edge Cases not handled" }
+            };
+
+            int noofColumns = ConsoleOutput.GetNumberofColumns(testinput);
+            Assert.True(noofColumns == 2);
+        }
+
+    }
+}
