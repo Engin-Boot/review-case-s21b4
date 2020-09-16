bae865d7d4758bff571c3eb12ebfd4b4d85199b7
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-09-16 22:29:06 +0530

    remove target

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ pushd "%~dp0"
 if exist Debug rd /s /q Debug
 if exist Release rd /s /q Release
 
-"%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" /t:Metrics
+"%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe"
 
 :exit
 popd
