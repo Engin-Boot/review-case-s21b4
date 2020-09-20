1032aba64c4cc1c01c11531feebfb0e5ee01d37d
Azimcharoliya <39900677+Azimcharoliya@users.noreply.github.com>
2020-09-20 16:19:18 +0530

    Update Analyser.cs

--- Receiver/Analyser.cs
+++ Receiver/Analyser.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -33,22 +34,55 @@ namespace Receiver
             return wordFrequency;
         }
 
+        
+        public string GetAssociatedDatesForAWord(IEnumerable<IEnumerable<string>> data, string word)
+        {
+            List<List<string>> dataList = (List<List<string>>)data;
+            string dates = "";
+            for(int i=0;i<dataList.Count;i++)
+            {
+                var wordsInARow = (List<string>)GetSeparatedWordsBySpaceFromARow(dataList[i]);
+                if (wordsInARow.Contains(word))
+                    dates += dataList[i][0] + " ";
+            }
+            
+            return dates;
+        }
+
         public IEnumerable<IEnumerable<string>> CountWordFrequency(IEnumerable<IEnumerable<string>> data)
         {
             IDictionary<string, int> wordFrequency = new Dictionary<string, int>();
 
-            foreach (var row in data)
+            var dataList = (List<List<string>>) data;
+            var originalData = dataList.Select(row => row.ToList()).ToList();
+
+            for (int i=0;i<dataList.Count;i++)
             {
-                var wordsInARow = GetSeparatedWordsBySpaceFromARow(row);
+                dataList[i].RemoveAt(0);
+                var wordsInARow = GetSeparatedWordsBySpaceFromARow(dataList[i]);
                 wordFrequency = AddWordCountInDictionary(wordFrequency, wordsInARow);
             }
 
             var wordCountList = wordFrequency.Select(item => new List<string> { item.Key, item.Value.ToString() }).ToList();
 
+            for (int i = 0; i < wordCountList.Count; i++)
+            {
+                wordCountList[i].Add(GetAssociatedDatesForAWord(originalData, wordCountList[i][0]));
+            }
+
+            for (int i = 0; i < wordCountList.Count; i++)
+            {
+                for (int j = 0; j < wordCountList[i].Count; j++)
+                {
+                    Console.Write(wordCountList[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+
             return wordCountList;
 
         }
-    }
 
+    }
 
 }
