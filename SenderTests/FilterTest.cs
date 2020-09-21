4ed7603508f38122f4e247060d1ee65c5e5424c2
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-09-21 13:33:29 +0530

    Update FilterTest.cs

--- SenderTests/FilterTest.cs
+++ SenderTests/FilterTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Sender;
 using Xunit;
@@ -6,18 +7,15 @@ namespace SenderTests
 {
     public class FilterTest
     {
+        readonly List<List<string>> testInput = new List<List<string>>(){
+            new List<string> { "column1", "column2", "column3", "column4" },
+            new List<string> { "c1data", "c2data", "c3data", "c4data" }
+        };
         [Fact]
         public void TestExpectingDataFilteredByColumnNosWhenCalledWithTwoDimensionalDataAndColumnNos()
         {
-            var testInput = new List<List<string>>();
-            var temp1 = new List<string> {"column1", "column2", "column3"};
-            testInput.Add(temp1);
 
-            var temp2 = new List<string> {"c1data", "c2data", "c3data"};
-            testInput.Add(temp2);
-            
-            Filter filter = new Filter();
-            var testOutput = (List<List<string>>) filter.GetDataFilteredByColumnNos(testInput, new List<int> {1, 2});
+            var testOutput = (List<List<string>>)Filter.GetDataFilteredByColumns(testInput, new List<int> { 1, 2 });
             Assert.Equal("column2", testOutput[0][0]);
             Assert.Equal("column3", testOutput[0][1]);
             Assert.Equal("c2data", testOutput[1][0]);
@@ -28,19 +26,20 @@ namespace SenderTests
         [Fact]
         public void TestExpectingDataFilteredByColumnRangeWhenCalledWithTwoDimensionalDataAndStartColAndEndCol()
         {
-            var testInput = new List<List<string>>();
-            var temp1 = new List<string> {"column1", "column2", "column3", "column4"};
-            testInput.Add(temp1);
+            
 
-            var temp2 = new List<string> {"c1data", "c2data", "c3data", "c4data"};
-            testInput.Add(temp2);
-
-            Filter filter = new Filter();
-            var testOutput = (List<List<string>>) filter.GetDataFilteredByColumnRange(testInput, 0, 2);
+            var testOutput = (List<List<string>>)Filter.GetDataFilteredByColumns(testInput, 0, 2);
             Assert.Equal("column1", testOutput[0][0]);
             Assert.Equal("column3", testOutput[0][2]);
             Assert.Equal("c1data", testOutput[1][0]);
             Assert.Equal("c3data", testOutput[1][2]);
+        }
+        [Fact]
+        public void TestExpectingIndexOutOfRangeExceptionWhenColumnNumberIsOutOfRange()
+        {
+            Assert.Throws<IndexOutOfRangeException>(() => Filter.GetDataFilteredByColumns(testInput, 0, 5));
+            Assert.Throws<IndexOutOfRangeException>(() => Filter.GetDataFilteredByColumns(testInput, new List<int> { 0, 5}));
+
         }
     }
 }
